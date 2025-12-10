fc97f382ffa87832e8c07883d9f12806c48eeb49
Muravev Ivan <muraviev.ivan04@gmail.com>
2025-12-11 00:57:21 +0300

    Added new objects

--- Assets/Scripts/ChangeObjects.cs
+++ Assets/Scripts/ChangeObjects.cs
@@ -1,28 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeObjects : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _cube;
-    [SerializeField]
-    private GameObject _sphere;
-    [SerializeField]
-    private GameObject _cylinder;
-    [SerializeField]
-    private GameObject _capsule;
+    private List<GameObject> _objects;
 
-    private Renderer _cubeRenderer;
-    private Renderer _sphereRenderer;
-    private Renderer _cylinderRenderer;
-    private Renderer _capsuleRenderer;
+    private List<Renderer> _renderers;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _cubeRenderer = _cube.GetComponent<Renderer>();
-        _sphereRenderer = _sphere.GetComponent<Renderer>();
-        _cylinderRenderer = _cylinder.GetComponent<Renderer>();
-        _capsuleRenderer = _capsule.GetComponent<Renderer>();
+        _renderers = new();
+        foreach (GameObject item in _objects)
+        {
+            _renderers.Add(item.GetComponent<Renderer>());
+        }
     }
 
     // Update is called once per frame
@@ -33,25 +26,25 @@ public class ChangeObjects : MonoBehaviour
 
     public void PaintToRed()
     {
-        _cubeRenderer.material.color = Color.red;
-        _sphereRenderer.material.color = Color.red;
-        _cylinderRenderer.material.color = Color.red;
-        _capsuleRenderer.material.color = Color.red;
+        foreach (Renderer item in _renderers)
+        {
+            item.material.color = Color.red;
+        }
     }
 
     public void PaintToGreen()
     {
-        _cubeRenderer.material.color = Color.green;
-        _sphereRenderer.material.color = Color.green;
-        _cylinderRenderer.material.color = Color.green;
-        _capsuleRenderer.material.color = Color.green;
+        foreach (Renderer item in _renderers)
+        {
+            item.material.color = Color.green;
+        }
     }
 
     public void PaintToBlue()
     {
-        _cubeRenderer.material.color = Color.blue;
-        _sphereRenderer.material.color = Color.blue;
-        _cylinderRenderer.material.color = Color.blue;
-        _capsuleRenderer.material.color = Color.blue;
+        foreach (Renderer item in _renderers)
+        {
+            item.material.color = Color.blue;
+        }
     }
 }
